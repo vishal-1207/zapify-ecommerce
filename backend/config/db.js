2da6c0ccd2a6cefb340bb69f7f096f98385f1b3e
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-10 12:51:01 +0530

    made changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
-import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
