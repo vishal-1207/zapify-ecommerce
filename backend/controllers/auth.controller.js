a92df05e9c5245f6a1ff2c621f8a4a02f9de78f5
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-10 12:51:29 +0530

    added register auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,30 +1,15 @@
-import db from "../models/user.model.js";
-import Op from "sequelize";
+import db from "../models/index.js";
+import * as authService from "../services/auth.service.js";
 
 const User = db.User;
 
 export const register = async (req, res) => {
-  const { username, email, password } = req.body;
-
   try {
-    const existingUser = await User.findOne({
-      where: { [Op.or]: [{ username }, { email }] },
-    });
-
-    if (existingUser) {
-      const message =
-        existingUser.username === username
-          ? "Username is already taken."
-          : "Email is already registered.";
-      return res.status(409).json({ message });
-    }
-
-    const newUser = await User.create({ username, email, password });
-    res
-      .status(201)
-      .json({ message: "User registered successfully.", user: newUser });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    const user = await authService.createUser(req.body);
+    res.status(201).json({ message: "User registered", user });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: err.message });
   }
 };
 
