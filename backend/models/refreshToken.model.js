17f94f386d3b8ace43b7c824512e849fa34fff23
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-12 00:18:00 +0530

    refresh token model added

--- backend/models/refreshToken.model.js
+++ backend/models/refreshToken.model.js
@@ -1,7 +1,6 @@
 export default (sequelize, DataTypes) => {
   const RefreshToken = sequelize.define("RefreshToken", {
-    tokenId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    accessToken: { type: DataTypes.STRING, allowNull: false },
+    tokenId: { type: DataTypes.STRING, primaryKey: true, allowNull: false },
     userId: { type: DataTypes.INTEGER, allowNull: false },
     expiresAt: { type: DataTypes.DATE, allowNull: false },
   });
