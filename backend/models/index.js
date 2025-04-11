49e5fa26c68c6ec17c34df647ec6761e3812795c
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-12 00:20:18 +0530

    token association added

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,5 +1,5 @@
-import { Sequelize, DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
+import { Sequelize } from "sequelize";
 
 import userModel from "./user.model.js";
 import productModel from "./product.model.js";
@@ -8,6 +8,7 @@ import cartModel from "./cart.model.js";
 import cartItemModel from "./cartItem.model.js";
 import orderModel from "./order.model.js";
 import orderItemModel from "./orderItem.model.js";
+import refreshTokenModel from "./refreshToken.model.js";
 
 const db = {};
 
@@ -21,31 +22,30 @@ db.Cart = cartModel(sequelize, Sequelize);
 db.CartItem = cartItemModel(sequelize, Sequelize);
 db.Order = orderModel(sequelize, Sequelize);
 db.OrderItem = orderItemModel(sequelize, Sequelize);
+db.RefreshToken = refreshTokenModel(sequelize, Sequelize);
 
-// 1. Category ↔ Product
 db.Category.hasMany(db.Product, { foreignKey: "categoryId" });
 db.Product.belongsTo(db.Category, { foreignKey: "categoryId" });
 
-// 2. User ↔ Cart
 db.User.hasOne(db.Cart, { foreignKey: "userId" });
 db.Cart.belongsTo(db.User, { foreignKey: "userId" });
 
-// 3. Cart ↔ CartItem ↔ Product
 db.Cart.hasMany(db.CartItem, { foreignKey: "cartId" });
 db.CartItem.belongsTo(db.Cart, { foreignKey: "cartId" });
 
 db.Product.hasMany(db.CartItem, { foreignKey: "productId" });
 db.CartItem.belongsTo(db.Product, { foreignKey: "productId" });
 
-// 4. User ↔ Order
 db.User.hasMany(db.Order, { foreignKey: "userId" });
 db.Order.belongsTo(db.User, { foreignKey: "userId" });
 
-// 5. Order ↔ OrderItem ↔ Product
 db.Order.hasMany(db.OrderItem, { foreignKey: "orderId" });
 db.OrderItem.belongsTo(db.Order, { foreignKey: "orderId" });
 
 db.Product.hasMany(db.OrderItem, { foreignKey: "productId" });
 db.OrderItem.belongsTo(db.Product, { foreignKey: "productId" });
+
+db.User.hasMany(db.RefreshToken, { foreignKey: "userId" });
+db.RefreshToken.belongsTo(db.User, { foreignKey: "userId" });
 
 export default db;
