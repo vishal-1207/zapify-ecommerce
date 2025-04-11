d1b47275befb3ec8f94821044ceda0393a5505a2
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-12 00:18:37 +0530

    created tokens utility

---
+++ backend/utils/token.utils.js
@@ -0,0 +1,34 @@
+import db from "../models/index.js";
+import { v4 as uuid4 } from "uuid";
+import jwt from "jsonwebtoken";
+
+const RefreshToken = db.RefreshToken;
+
+export const generateAccessToken = async (userData) => {
+  const accessToken = jwt.sign(
+    { id: userData?.id, role: userData?.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "15m",
+    }
+  );
+  return accessToken;
+};
+
+export const generateRefreshToken = async (userData) => {
+  const tokenId = uuid4();
+  const expiresAt = new Date(Date.now(+7 * 24 * 60 * 60 * 1000));
+
+  const token = jwt.sign(
+    { tokenId, id: userData?.id, role: userData?.role },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  await RefreshToken.create({
+    tokenId,
+    userId: userData?.id,
+    expiresAt,
+  });
+  return token;
+};
