f83674181a09a77f0d6655cf785e8e35ef549292
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-12 00:22:26 +0530

    routes updated and added rate limiter

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  refreshTokenHander,
+  register,
+} from "../controllers/auth.controller.js";
+import rateLimit from "express-rate-limit";
 
 const router = express.Router();
 
-// router.post("/register", register);
+const refreshLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: "Too many requests.",
+});
+
 router.route("/register").post(register);
-// router.post("/login", login);
 router.route("/login").post(login);
-// router.post('/logout')
+router.route("/access-token").post(refreshLimiter, refreshTokenHander);
+router.route("/logout").post(logout);
 
 export default router;
