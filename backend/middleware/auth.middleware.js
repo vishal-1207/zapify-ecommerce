0df35dd4d0e35f01cc5f129805ca51d165836af3
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-12 00:23:39 +0530

    added authenticate middleware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+export const authenticate = async (req, res, next) => {
+  try {
+    const token =
+      req.cookie?.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({ message: "Access token missing." });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    console.log("Auth middleare error: ", error.message);
+    res.status(403).json({ message: "Invalid or expire token." });
+  }
+};
