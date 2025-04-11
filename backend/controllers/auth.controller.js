8041eda27660a515c8d8840c8d5c8f3714c6592a
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-12 00:21:49 +0530

    token handler and logout function added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,10 @@
 import db from "../models/index.js";
 import * as authService from "../services/auth.service.js";
+import jwt from "jsonwebtoken";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "../utils/token.utils.js";
 
 export const register = async (req, res) => {
   try {
@@ -13,10 +18,60 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   try {
-    const result = await authService.findUser(req.body);
-    res.status(200).json({ message: "Login successfull", result });
+    const { accessToken, user } = await authService.findUser(req.body);
+    const refreshToken = await generateRefreshToken(user.id, user.role);
+    res
+      .cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "Strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      })
+      .status(200)
+      .json({ message: "Login successfull", accessToken, refreshToken, user });
   } catch (err) {
     console.log(err);
     res.status(401).json({ message: err.message });
+  }
+};
+
+export const refreshTokenHander = async (req, res) => {
+  const { refreshToken } = req.cookies.refreshToken;
+  try {
+    const payload = jwt.verfy(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    const storedToken = await db.RefreshToken.findByPk(payload.tokenId);
+    if (!storedToken) throw new Error("Invalid refresh token");
+
+    await storedToken.destroy();
+
+    const accessToken = generateAccessToken(payload.userId);
+    const newRefreshToken = await generateRefreshToken(payload.userId);
+    res.json({ accessToken, refreshToken: newRefreshToken });
+  } catch (error) {
+    res.status(403).json({ message: "Ivalid or expired token." });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+    });
+
+    const payload = await jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    await db.RefreshToken.destroy({ where: { tokenId: payload.tokenId } });
+
+    res.status(200).json({ message: "Logged out successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: "Logout failed." });
   }
 };
