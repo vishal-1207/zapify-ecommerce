be2adc9f10defdb2b491994dad670bbae48dc4ba
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-15 23:58:32 +0530

    added user routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import db from "./models/index.js";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world.");
@@ -20,7 +22,7 @@ app.get("/", (req, res) => {
 
 app.use(errorHandler);
 
-db.sequelize.sync({ force: true }).then(() => {
+db.sequelize.sync({ force: false }).then(() => {
   console.log("Database synced...");
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
