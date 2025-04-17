0beec71b2c36dc32244e9b189ec163f60561f830
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-17 23:57:46 +0530

    added dest and filename

--- backend/middleware/multer.middleware.js
+++ backend/middleware/multer.middleware.js
@@ -1,7 +1,10 @@
 import multer from "multer";
 import path from "path";
 
-const storage = multer.diskStorage({});
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "./uploads"),
+  filename: (req, file, cb) => cb(null, Date.now() + "_" + file.originalname),
+});
 
 const fileFilter = (req, file, cb) => {
   const ext = path.extname(file.originalname).toLowerCase();
