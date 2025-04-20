f3696747bc5625582f6f1198bfb2ff140352876d
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-20 23:23:26 +0530

    added limit option in multer

--- backend/middleware/multer.middleware.js
+++ backend/middleware/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "./uploads"),
+  destination: (req, file, cb) => cb(null, "./uploads/"),
   filename: (req, file, cb) => cb(null, Date.now() + "_" + file.originalname),
 });
 
@@ -18,4 +18,7 @@ const fileFilter = (req, file, cb) => {
 export const upload = multer({
   storage,
   fileFilter,
+  limits: {
+    fileSize: 100 * 1024 * 1024,
+  },
 });
