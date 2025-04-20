f866b28ab289e6147c16b3e156fb7128513b7b7b
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-20 23:24:46 +0530

    added joi validation

---
+++ backend/utils/validationSchema.js
@@ -0,0 +1,61 @@
+import Joi from "joi";
+
+export const registerSchema = Joi.object({
+  username: Joi.string().min(6).required().messages({
+    "string.empty": "Name is required.",
+    "string.min": "Name must be atleast 6 characters long.",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.empty": "Email is required.",
+    "string.email": "Email format is invalid.",
+  }),
+  password: Joi.string().min(8).required().messages({
+    "string.empty": "Password is required.",
+    "string.min": "Password must be atleast 8 characters long.",
+  }),
+});
+
+export const loginSchema = Joi.object({
+  userId: Joi.string()
+    .custom((value, helpers) => {
+      const emailRegex = /^[w-]+(.[w-]+)*@([w-]+.)+[a-zA-Z]{2,7}$/;
+      const isEmail = emailRegex.test(value);
+
+      const isUsername = /^[a-zA-Z]{6,}$/;
+
+      if (!isEmail && !isUsername) {
+        return helpers.error("any.invalid");
+      }
+
+      return value;
+    })
+    .required()
+    .messages({
+      "string.empty": "Username or Email is required.",
+      "any.invalid": "User ID must be either a username or a email.",
+    }),
+
+  password: Joi.string().min(8).required().messages({
+    "string.empty": "Password is required.",
+    "string.min": "Passwod is incorrect.",
+  }),
+});
+
+export const productSchema = Joi.object({
+  name: Joi.string().min(10).required().messages({
+    "string.empty": "Product name is required.",
+    "string.min": "Product name must be atleast 6 characters long.",
+  }),
+
+  description: Joi.string().min(50).required().messages({
+    "string.empty": "Description is required.",
+    "string.min": "Description must be atleast 50 characters long.",
+  }),
+
+  price: Joi.number().precision(2).required().messages({
+    "number.base": "Price must be a valid number.",
+    "number.empty": "Price is required.",
+    "number.positive": "Price must be a positive value.",
+    "number.precision": "Price can have a maximum of 2 decimal places.",
+  }),
+});
