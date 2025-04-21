65bb321092a943af0c835defd7e7c874bea126a6
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-21 23:51:01 +0530

    added csrf middleware

---
+++ backend/middleware/csrf.middleware.js
@@ -0,0 +1,15 @@
+import { verifyCSRF } from "../utils/csrf.utils.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+export const csrfProtection = asyncHandler(async (req, res, next) => {
+  const token = req.headers["x-csrf-token"];
+  const secret = req.cookies.csrf_secret;
+
+  if (!token || !secret || !verifyCSRF(secret, token)) {
+    return res.status(403).json({
+      message: "Invalid CSRF token.",
+    });
+  }
+
+  next();
+});
