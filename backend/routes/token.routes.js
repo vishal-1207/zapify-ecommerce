bfa3e42d6364e2d016ac479ec4b673a65eba0d18
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-21 23:51:32 +0530

    added csrf token route

---
+++ backend/routes/token.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { csrfToken } from "../controllers/token.controller.js";
+import { csrfProtection } from "../middleware/csrf.middleware.js";
+
+const router = express.Router();
+
+router.route("/csrf-token", csrfProtection, csrfToken);
+
+export default tokenRoutes;
