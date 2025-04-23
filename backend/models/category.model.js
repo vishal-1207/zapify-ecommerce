73332c90794ab99056a69415f5de3bbbe2539517
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-23 23:56:22 +0530

    added image url column in category model

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -2,6 +2,7 @@ export default (sequelize, DataTypes) => {
   const Category = sequelize.define("Category", {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false, unique: true },
+    imageUrl: { type: DataTypes.STRING, allowNull: false },
   });
   return Category;
 };
