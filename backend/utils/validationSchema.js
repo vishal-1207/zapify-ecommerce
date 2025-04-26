8fd79cdf5f61573e64fa2fd11ca9910248218e05
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-27 01:36:22 +0530

    added joi and xss sanitization

--- backend/utils/validationSchema.js
+++ backend/utils/validationSchema.js
@@ -1,27 +1,42 @@
 import Joi from "joi";
+import xss from "xss";
+
+const sanitize = (value) => xss(value);
 
 export const registerSchema = Joi.object({
-  username: Joi.string().min(6).required().messages({
-    "string.empty": "Name is required.",
-    "string.min": "Name must be atleast 6 characters long.",
-  }),
-  email: Joi.string().email().required().messages({
-    "string.empty": "Email is required.",
-    "string.email": "Email format is invalid.",
-  }),
-  password: Joi.string().min(8).required().messages({
-    "string.empty": "Password is required.",
-    "string.min": "Password must be atleast 8 characters long.",
-  }),
+  username: Joi.string()
+    .min(6)
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Name is required.",
+      "string.min": "Name must be atleast 6 characters long.",
+    }),
+  email: Joi.string()
+    .email()
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Email is required.",
+      "string.email": "Email format is invalid.",
+    }),
+  password: Joi.string()
+    .min(8)
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Password is required.",
+      "string.min": "Password must be atleast 8 characters long.",
+    }),
 });
 
 export const loginSchema = Joi.object({
   userId: Joi.string()
     .custom((value, helpers) => {
-      const emailRegex = /^[w-]+(.[w-]+)*@([w-]+.)+[a-zA-Z]{2,7}$/;
+      const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
       const isEmail = emailRegex.test(value);
 
-      const isUsername = /^[a-zA-Z]{6,}$/;
+      const isUsername = /^[a-zA-Z]{5,}$/.test(value);
 
       if (!isEmail && !isUsername) {
         return helpers.error("any.invalid");
@@ -30,27 +45,44 @@ export const loginSchema = Joi.object({
       return value;
     })
     .required()
+    .custom(sanitize, "XSS Sanitization")
     .messages({
       "string.empty": "Username or Email is required.",
       "any.invalid": "User ID must be either a username or a email.",
     }),
 
-  password: Joi.string().min(8).required().messages({
-    "string.empty": "Password is required.",
-    "string.min": "Passwod is incorrect.",
-  }),
+  password: Joi.string()
+    .min(8)
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Password is required.",
+      "string.min": "Passwod is incorrect.",
+    }),
 });
 
 export const productSchema = Joi.object({
-  name: Joi.string().trim().min(10).max(100).required().messages({
-    "string.empty": "Product name is required.",
-    "string.min": "Product name must be atleast 6 characters long.",
-  }),
+  name: Joi.string()
+    .trim()
+    .min(10)
+    .max(100)
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Product name is required.",
+      "string.min": "Product name must be atleast 6 characters long.",
+    }),
 
-  description: Joi.string().trim().max(1000).required().messages({
-    "string.empty": "Description is required.",
-    "string.min": "Description must be atleast 50 characters long.",
-  }),
+  description: Joi.string()
+    .trim()
+    .min(50)
+    .max(1000)
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Description is required.",
+      "string.min": "Description must be atleast 50 characters long.",
+    }),
 
   price: Joi.number().positive().precision(2).required().messages({
     "number.base": "Price must be a valid number.",
@@ -69,9 +101,15 @@ export const productSchema = Joi.object({
 });
 
 export const categorySchema = Joi.object({
-  name: Joi.string().trim().min(4).max(15).required().messages({
-    "string:empty": "Category name is required.",
-    "string.min": "Category name should be atleast 4 characters long.",
-    "string.max": "Category name cannot exceed 15 characters.",
-  }),
+  name: Joi.string()
+    .trim()
+    .min(4)
+    .max(15)
+    .required()
+    .custom(sanitize, "XSS Sanitization")
+    .messages({
+      "string.empty": "Category name is required.",
+      "string.min": "Category name should be atleast 4 characters long.",
+      "string.max": "Category name cannot exceed 15 characters.",
+    }),
 });
