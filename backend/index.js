5d80aafdac8f71d892391b8572b52bd76764d7b2
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-29 20:37:34 +0530

    added admin and category routes

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
 import productRoutes from "./routes/product.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 import tokenRoutes from "./routes/token.routes.js";
 
@@ -19,9 +21,10 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/token", tokenRoutes);
+app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/products", productRoutes);
-// app.use("/api/categories");
+app.use("/api/categories", categoryRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world.");
