f2dd5b7d08177d7f677322136f9a57ae4179f026
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-29 20:33:58 +0530

    updated category model

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -2,8 +2,6 @@ export default (sequelize, DataTypes) => {
   const Category = sequelize.define("Category", {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false, unique: true },
-    imageUrl: { type: DataTypes.STRING, allowNull: false },
-    imagePublicId: { type: DataTypes.STRING, allowNull: false },
   });
   return Category;
 };
