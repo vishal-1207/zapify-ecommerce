7b3a324e4f953693e98bbece9613296c22a46ea9
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-04-29 20:36:38 +0530

    added more status value

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -1,9 +1,16 @@
 export default (sequelize, DataTypes) => {
   const Order = sequelize.define("Order", {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    totalAmount: { type: DataTypes.FLOAT, allowNull: false },
+    totalAmount: { type: DataTypes.DECIMAL(10, 2), allowNull: false },
     status: {
-      type: DataTypes.ENUM("pending", "shipped", "paid"),
+      type: DataTypes.ENUM(
+        "pending",
+        "paid",
+        "shipped",
+        "delivered",
+        "cancelled",
+        "refunded"
+      ),
       defaultValue: "pending",
     },
   });
