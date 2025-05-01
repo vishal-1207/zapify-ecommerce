5a826666671d429e673576b877eb19ff95b37202
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-01 22:22:56 +0530

    added before create and before update hook

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,7 +1,37 @@
+import { Op } from "sequelize";
+import slugify from "slugify";
+
 export default (sequelize, DataTypes) => {
   const Category = sequelize.define("Category", {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     name: { type: DataTypes.STRING, allowNull: false, unique: true },
+    slug: { type: DataTypes.STRING, allowNull: false, unique: true },
+  });
+
+  Category.beforeCreate(async (category, options) => {
+    const baseSlug = slugify(category.name, { lower: true, strict: true });
+
+    const existing = await Category.findOne({ where: { slug: baseSlug } });
+    if (existing) {
+      throw new Error("Slug already exists for another category.");
+    }
+
+    category.slug = baseSlug;
+  });
+
+  Category.beforeUpdate(async (category, options) => {
+    if (category.changed("name")) {
+      const baseSlug = slugify(category.name, { lower: true, strict: true });
+
+      const existing = await Category.findOne({
+        where: { slug: baseSlug, id: { [Op.ne]: category.id } },
+      });
+      if (existing) {
+        throw new Error("Slug already exists for another category.");
+      }
+
+      category.slug = baseSlug;
+    }
   });
   return Category;
 };
