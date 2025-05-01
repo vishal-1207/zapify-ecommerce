b60804d47c6a9d72969b1f61bad623442fe8c96e
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-01 22:25:09 +0530

    added before create and update hook

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,3 +1,7 @@
+import { nanoid } from "nanoid";
+import { Op } from "sequelize";
+import slugify from "slugify";
+
 export default (sequelize, DataTypes) => {
   const Product = sequelize.define("Product", {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -5,7 +9,36 @@ export default (sequelize, DataTypes) => {
     description: { type: DataTypes.TEXT, allowNull: false },
     price: { type: DataTypes.FLOAT, allowNull: false },
     stock: { type: DataTypes.INTEGER, defaultValue: 0 },
-    productSlug: { type: DataTypes.STRING, allowNull: false, unique: true },
+    slug: { type: DataTypes.STRING, allowNull: false, unique: true },
   });
+
+  Product.beforeCreate(async (product, options) => {
+    const baseSlug = slugify(product.name, { lower: true, strict: true });
+    let finalSlug = `${baseSlug}-${nanoid(6)}`;
+    const exists = await Product.findOne({ where: { slug: finalSlug } });
+    if (exists) {
+      finalSlug = `${baseSlug}-${nanoid(6)}`;
+    }
+
+    product.slug = finalSlug;
+  });
+
+  Product.beforeUpdate(async (product, options) => {
+    if (product.changed("name")) {
+      const baseSlug = slugify(product.name, { lower: true, strict: true });
+      let finalSlug = `${baseSlug}-${nanoid(6)}`;
+
+      const exists = await Product.findOne({
+        where: { slug: finalSlug, id: { [Op.ne]: product.id } },
+      });
+
+      if (exists) {
+        finalSlug = `${baseSlug}-${nanoid(6)}`;
+      }
+
+      product.slug = finalSlug;
+    }
+  });
+
   return Product;
 };
