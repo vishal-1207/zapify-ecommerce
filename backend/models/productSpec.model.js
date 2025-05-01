0991ffbbb04587c8d6b4d2822f0d16db7dbbf603
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-01 22:24:43 +0530

    added product specs model

---
+++ backend/models/productSpec.model.js
@@ -0,0 +1,7 @@
+export default (sequelize, DataTypes) => {
+  const ProductSpec = sequelize.define("ProductSpec", {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    key: { type: DataTypes.STRING, allowNull: false },
+    value: { type: DataTypes.STRING, allowNull: false },
+  });
+};
