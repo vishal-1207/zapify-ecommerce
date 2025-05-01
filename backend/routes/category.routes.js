14799079866280b2e18ef907b28d8a580f9dfd48
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-01 22:23:43 +0530

    added update and delete route

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import {
   addCategory,
+  deleteCategory,
   getCategories,
+  getcategoryProducts,
+  updateCategory,
 } from "../controllers/category.controller.js";
 import { authenticate, isAdmin } from "../middleware/auth.middleware.js";
 import { csrfProtection } from "../middleware/csrf.middleware.js";
@@ -19,5 +22,18 @@ router
     upload.single("image"),
     addCategory
   );
+
+router.route("/:slug/products").get(getcategoryProducts);
+
+router
+  .route("/:id")
+  .put(
+    authenticate,
+    isAdmin,
+    csrfProtection,
+    upload.single("image"),
+    updateCategory
+  )
+  .delete(authenticate, isAdmin, csrfProtection, deleteCategory);
 
 export default router;
