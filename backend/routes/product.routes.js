69fa6a103cbd17097510ed8ae9d29d8b365283f0
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-04 21:55:04 +0530

    added product routes

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,11 +1,26 @@
 import express from "express";
+import { authenticate, isAdmin } from "../middleware/auth.middleware";
+import { csrfProtection } from "../middleware/csrf.middleware";
+import { upload } from "../middleware/multer.middleware";
+import {
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "../controllers/product.controller";
 
 const router = express.Router();
 
-router.route("/").get();
-router.route("/:id").get();
-router.route("/").post();
-router.route("/:id").put();
-router.route("/:id").delete();
+// router.route("/").get();
+// router.route("/product/:slug").get();
+
+router.route("/id/:id").get(isAdmin);
+router
+  .route("/")
+  .post(authenticate, isAdmin, csrfProtection, upload.array(), createProduct);
+
+router
+  .route("/id/:id")
+  .put(authenticate, isAdmin, csrfProtection, upload.array(), updateProduct)
+  .delete(authenticate, isAdmin, csrfProtection, deleteProduct);
 
 export default router;
