ed79a76901e74b2eaa3ba3922f558d6479f9d2c3
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-06 23:46:18 +0530

    made changes

--- backend/models/productSpec.model.js
+++ backend/models/productSpec.model.js
@@ -3,11 +3,6 @@ export default (sequelize, DataTypes) => {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     key: { type: DataTypes.STRING, allowNull: false },
     value: { type: DataTypes.STRING, allowNull: false },
-    productId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: { model: "Product", key: "id" },
-    },
   });
   return ProductSpec;
 };
