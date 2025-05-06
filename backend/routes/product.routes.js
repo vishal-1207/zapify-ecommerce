4e7da990b28bbd2bbb2906e723fe65d8988a9deb
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-06 23:49:53 +0530

    added routes with proper middleware and controller

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,26 +1,42 @@
 import express from "express";
-import { authenticate, isAdmin } from "../middleware/auth.middleware";
-import { csrfProtection } from "../middleware/csrf.middleware";
-import { upload } from "../middleware/multer.middleware";
+import { authenticate, isAdmin } from "../middleware/auth.middleware.js";
+import { csrfProtection } from "../middleware/csrf.middleware.js";
+import { upload } from "../middleware/multer.middleware.js";
 import {
   createProduct,
   deleteProduct,
   updateProduct,
-} from "../controllers/product.controller";
+} from "../controllers/product.controller.js";
 
 const router = express.Router();
 
 // router.route("/").get();
 // router.route("/product/:slug").get();
 
-router.route("/id/:id").get(isAdmin);
-router
-  .route("/")
-  .post(authenticate, isAdmin, csrfProtection, upload.array(), createProduct);
+// router.route("/id/:id").get(isAdmin);
+router.route("/").post(
+  authenticate,
+  isAdmin,
+  csrfProtection,
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "gallery", maxCount: 10 },
+  ]),
+  createProduct
+);
 
 router
   .route("/id/:id")
-  .put(authenticate, isAdmin, csrfProtection, upload.array(), updateProduct)
+  .put(
+    authenticate,
+    isAdmin,
+    csrfProtection,
+    upload.fields([
+      { name: "thumbnail", maxCount: 1 },
+      { name: "gallery", maxCount: 10 },
+    ]),
+    updateProduct
+  )
   .delete(authenticate, isAdmin, csrfProtection, deleteProduct);
 
 export default router;
