81666cd1d171f4d163f987d283b9d0e29316bf5a
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-12 23:30:23 +0530

    added token column

--- backend/models/refreshToken.model.js
+++ backend/models/refreshToken.model.js
@@ -2,6 +2,7 @@ export default (sequelize, DataTypes) => {
   const RefreshToken = sequelize.define("RefreshToken", {
     tokenId: { type: DataTypes.STRING, primaryKey: true, allowNull: false },
     userId: { type: DataTypes.INTEGER, allowNull: false },
+    token: { type: DataTypes.STRING, allowNull: false },
     expiresAt: { type: DataTypes.DATE, allowNull: false },
   });
   return RefreshToken;
