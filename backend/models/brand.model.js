a54ffaa88dcd4443c1dc14be39153e4e67ae0c05
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-23 23:15:41 +0530

    added brand model

--- backend/models/brand.model.js
+++ backend/models/brand.model.js
@@ -1,7 +1,8 @@
 export default (sequelize, DataTypes) => {
   const Brand = sequelize.define("Brand", {
     id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
-    name: { type: DataTypes.STRING, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false, unique: true },
+    description: { type: DataTypes.TEXT, allowNull: false },
   });
 
   return Brand;
