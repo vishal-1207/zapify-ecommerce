294ca576b493cbbf1f74d229dc51a2945ef15e0c
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-24 23:50:02 +0530

    added brand routes

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
+import brandRoutes from "./routes/brand.routes.js";
 import { errorHandler } from "./middleware/error.middleware.js";
 import tokenRoutes from "./routes/token.routes.js";
 
@@ -23,6 +24,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/token", tokenRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/brand", brandRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/categories", categoryRoutes);
 
@@ -34,7 +36,7 @@ app.use(errorHandler);
 
 const startServer = async () => {
   try {
-    await db.sequelize.sync({ force: true, alter: true });
+    await db.sequelize.sync({ force: false });
     console.log("Database synced...");
 
     app.listen(PORT, () => {
