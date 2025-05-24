537861cab2da8a6a151689e94378895ab0e465c7
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-24 23:54:46 +0530

    utilized validate middlware

--- backend/routes/admin.routes.js
+++ backend/routes/admin.routes.js
@@ -2,10 +2,14 @@ import express from "express";
 import { login, logout } from "../controllers/auth.controller.js";
 import { authenticate } from "../middleware/auth.middleware.js";
 import { csrfProtection } from "../middleware/csrf.middleware.js";
+import { validate } from "../middleware/validate.middleware.js";
+import { loginSchema, registerSchema } from "../utils/validationSchema.js";
 
 const router = express.Router();
 
-router.route("/login").post(csrfProtection, login);
-router.route("/logout").post(authenticate, csrfProtection, logout);
+router.route("/login").post(csrfProtection, validate(loginSchema), login);
+router
+  .route("/logout")
+  .post(authenticate, validate(registerSchema), csrfProtection, logout);
 
 export default router;
