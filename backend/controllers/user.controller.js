d01a190c191db317b1c7ac411ed2304031f9e2ce
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-25 23:52:52 +0530

    refactored user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,18 +1,15 @@
 import db from "../models/index.js";
+import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 export const currentUserDetails = asyncHandler(async (req, res) => {
-  try {
-    const user = await db.User.findByPk(req.user.id, {
-      attributes: { exclude: ["password"] },
-    });
+  const user = await db.User.findByPk(req.user.id, {
+    attributes: { exclude: ["password"] },
+  });
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found." });
-    }
-
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Server error." });
+  if (!user) {
+    throw new ApiError(404, "User not found.");
   }
+
+  res.json({ message: "User details fetched successfully.", user });
 });
