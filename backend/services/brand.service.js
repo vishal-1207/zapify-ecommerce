74f30550b54a2ad0b8c796419bf48000e432f902
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-05-30 23:58:14 +0530

    added update brand service

--- backend/services/brand.service.js
+++ backend/services/brand.service.js
@@ -5,6 +5,7 @@ import uploadToCloudinary from "../utils/cloudinary.util.js";
 const Brand = db.Brand;
 const Media = db.Media;
 
+// GET BRAND SERVICE
 export const getBrandService = async (id) => {
   try {
     const brand = await Brand.findByPk(id);
@@ -45,4 +46,61 @@ export const addBrandService = async (data, file) => {
   });
 
   return { brand, media };
+};
+
+export const updateBrandService = async (id, data, file) => {
+  const { name, description } = data;
+
+  const existingBrand = await Brand.findByPk(id);
+  if (!existingBrand) {
+    throw new ApiError(404, "Brand not found.");
+  }
+
+  if (name) {
+    const brand = await Brand.findOne({
+      where: { name },
+    });
+
+    if (brand && brand.id !== id) {
+      throw new ApiError(409, "Brand with this name already exists.");
+    }
+
+    brand.name = name;
+    brand.description = description || brand.description;
+    await brand.save();
+
+    if (file) {
+      const existingMedia = await Media.findOne({
+        where: {
+          associatedType: "brand",
+          associatedId: id,
+        },
+      });
+
+      if (existingMedia) {
+        if (existingMedia.publicId) {
+          await cloudinary.uploader.destroy(existingMedia.publicId, {
+            resource_type: "image",
+          });
+        }
+        await existingMedia.destroy();
+      }
+    }
+
+    const uploadResult = await uploadToCloudinary(
+      image.path,
+      process.env.CLOUDINARY_BRAND_FOLDER
+    );
+
+    media = await Media.create({
+      publicId: uploadResult.public_id,
+      url: uploadResult.secure_url,
+      fileType: uploadResult.resource_type,
+      tag: "thumbnail",
+      associatedType: "brand",
+      associatedId: category.id,
+    });
+
+    return brand;
+  }
 };
