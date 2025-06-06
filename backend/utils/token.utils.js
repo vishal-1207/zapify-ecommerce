815f28097c752a00084654d7685f05f070bd5504
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-06 22:24:54 +0530

    fixed parameter type

--- backend/utils/token.utils.js
+++ backend/utils/token.utils.js
@@ -4,14 +4,14 @@ import jwt from "jsonwebtoken";
 
 const RefreshToken = db.RefreshToken;
 
-export const generateAccessToken = ({ id, role }) => {
+export const generateAccessToken = (id, role) => {
   const accessToken = jwt.sign({ id, role }, process.env.JWT_SECRET, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
   });
   return accessToken;
 };
 
-export const generateRefreshToken = async ({ id, role }) => {
+export const generateRefreshToken = async (id, role) => {
   const tokenId = uuid4();
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
 
