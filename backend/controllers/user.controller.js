b17b8f379d4e82e69566e2aaca18bfffa342bb54
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-11 23:56:02 +0530

    update profile function added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -2,6 +2,8 @@ import db from "../models/index.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
+const User = db.User;
+
 export const currentUserDetails = asyncHandler(async (req, res) => {
   const user = await db.User.findByPk(req.user.id, {
     attributes: { exclude: ["password"] },
@@ -12,4 +14,23 @@ export const currentUserDetails = asyncHandler(async (req, res) => {
   }
 
   res.json({ message: "User details fetched successfully.", user });
+});
+
+//DUMMY FUNCTION TO UPDATE PROFILE
+
+export const updateProfile = asyncHandler(async (req, res) => {
+  const { name, email } = req.body;
+  const user = await User.findByPk(req.user.id);
+
+  if (!user) {
+    throw new ApiError(404, "User not found.");
+  }
+
+  if (name) user.name = name;
+  if (email) user.email = email;
+  await user.save();
+  res.status(200).json({
+    message: "Profile updated successfully.",
+    user,
+  });
 });
