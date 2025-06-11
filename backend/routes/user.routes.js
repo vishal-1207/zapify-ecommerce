135a29f02d4737f4e583ce310cd6bd4f512b6730
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-11 23:56:21 +0530

    added and fixed routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
 import { currentUserDetails } from "../controllers/user.controller.js";
+import { csrfProtection } from "../middleware/csrf.middleware.js";
+import { updateProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.route("/me").post(authenticate, currentUserDetails);
+router.route("/me").get(authenticate, currentUserDetails);
+router.route("/edit").put(authenticate, csrfProtection, updateProfile);
 
 export default router;
