123491fc6f089f0a8dbabf39ed4ac9a85116c2d7
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-24 23:45:54 +0530

    added support for multi roles

--- backend/services/auth.service.js
+++ backend/services/auth.service.js
@@ -1,12 +1,13 @@
 import db from "../models/index.js";
 import { Op } from "sequelize";
 import bcrypt from "bcrypt";
-import { generateAccessToken } from "../utils/token.utils.js";
+import generateTokens from "../utils/token.utils.js";
 import ApiError from "../utils/ApiError.js";
+import setTokensInCookies from "../utils/setTokensInCookies.js";
 
 const User = db.User;
 
-export const createUser = async (userData) => {
+export const createUser = async (userData, res) => {
   const { fullname, username, email, password } = userData;
 
   const existingUser = await User.findOne({
@@ -32,12 +33,19 @@ export const createUser = async (userData) => {
     username,
     email,
     password: hashedPassword,
+    roles: ["user"],
+    provider: "local",
   });
 
-  return user;
+  const payload = { userId: user.id, roles: user.roles };
+  const tokens = generateTokens(payload);
+
+  setTokensInCookies(res, tokens);
+
+  return { user, accessToken: tokens.accessToken };
 };
 
-export const findUser = async (userData) => {
+export const findUser = async (userData, res) => {
   const { userId, password } = userData;
   const user = await User.findOne({
     where: {
@@ -45,25 +53,21 @@ export const findUser = async (userData) => {
     },
   });
 
-  if (!user) {
-    throw new ApiError(404, "User not found");
-  }
+  if (!user) throw new ApiError(404, "User not found.");
+
+  if (user.provider !== "local")
+    throw new ApiError(400, `Login via ${user.provider} account.`);
 
   const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new ApiError(401, "Invalid credentials.");
 
-  if (!isMatch) {
-    throw new ApiError(401, "Invalid credentials");
-  }
-
-  const accessToken = generateAccessToken(user.id, user.role);
-
-  return {
-    accessToken,
-    user: {
-      id: user.id,
-      username: user.username,
-      email: user.email,
-      role: user.role,
-    },
+  const payload = {
+    userId: user.id,
+    roles: user.roles,
   };
+
+  const tokens = generateTokens(payload);
+  setTokensInCookies(res, tokens);
+
+  return { user, accessToken: tokens.accessToken };
 };
