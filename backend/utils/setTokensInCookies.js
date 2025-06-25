71d99aeffb9f912649e860d89054c6c163a33439
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-25 23:25:26 +0530

    added cookie utility

---
+++ backend/utils/setTokensInCookies.js
@@ -0,0 +1,15 @@
+export default setTokensInCookies = (res, { accessToken, refreshToken }) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "Strict",
+    maxAge: 15 * 60 * 1000,
+  });
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "Strict",
+    maxAge: 10 * 24 * 60 * 60 * 1000,
+  });
+};
