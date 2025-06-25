2adb99a8649c48e9892319771c89606444118793
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-25 23:25:07 +0530

    merged two functions into one

--- backend/utils/token.utils.js
+++ backend/utils/token.utils.js
@@ -4,28 +4,27 @@ import jwt from "jsonwebtoken";
 
 const RefreshToken = db.RefreshToken;
 
-export const generateAccessToken = (id, role) => {
-  const accessToken = jwt.sign({ id, role }, process.env.JWT_SECRET, {
+export default generateTokens = async ({ userId, roles }) => {
+  const accessToken = jwt.sign({ userId, roles }, process.env.JWT_SECRET, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
   });
-  return accessToken;
-};
 
-export const generateRefreshToken = async (id, role) => {
   const tokenId = uuid4();
   const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-
-  const token = jwt.sign(
-    { tokenId, id, role },
+  const refreshToken = jwt.sign(
+    { tokenId, userId, roles },
     process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
   );
 
   await RefreshToken.create({
     tokenId,
-    userId: id,
-    token,
+    userId,
+    token: refreshToken,
     expiresAt,
   });
-  return token;
+
+  return { accessToken, refreshToken };
 };
