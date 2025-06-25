e0bbbfcdce4f7aa92fd2a4e1af7d54146b0e1f22
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-06-25 23:26:18 +0530

    fixed auth controller functions

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,68 +2,61 @@ import db from "../models/index.js";
 import { createUser, findUser } from "../services/auth.service.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
-import {
-  generateAccessToken,
-  generateRefreshToken,
-} from "../utils/token.utils.js";
 import ApiError from "../utils/ApiError.js";
 
 export const register = asyncHandler(async (req, res) => {
   const { fullname, username, email, password } = req.body;
-  const user = await createUser({
-    fullname,
-    username,
-    email,
-    password,
-  });
+  const { user, accessToken } = await createUser(
+    {
+      fullname,
+      username,
+      email,
+      password,
+    },
+    res
+  );
 
-  res.status(201).json({ message: "User registered", user });
+  res.status(201).json({ message: "User registered", user, accessToken });
 });
 
 export const login = asyncHandler(async (req, res) => {
   const { userId, password } = req.body;
-
-  const { accessToken, user } = await findUser({
-    userId,
-    password,
-  });
-
-  const refreshToken = await generateRefreshToken(user.id, user.role);
-  res
-    .cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: "Strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    })
-    .status(200)
-    .json({ message: "Login successfull", accessToken, user });
+  const { user, accessToken } = await findUser({ userId, password }, res);
+  res.status(200).json({ message: "Login successfull", user, accessToken });
 });
 
 export const refreshTokenHander = asyncHandler(async (req, res) => {
-  const refreshToken = req.cookies.refreshToken || req.body.refreshToken;
-  if (!refreshToken) {
+  const oldRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+  if (!oldRefreshToken) {
     throw new ApiError(401, "Unauthorized access.");
   }
-  const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+  const payload = jwt.verify(oldRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+  if (!payload) {
+    throw new ApiError(401, "Invalid or expired refresh token.");
+  }
 
   const storedToken = await db.RefreshToken.findByPk(payload.tokenId);
   if (!storedToken) throw new Error("Invalid refresh token");
 
   await storedToken.destroy();
 
-  const accessToken = generateAccessToken(payload.userId);
-  const newRefreshToken = await generateRefreshToken(payload.userId);
+  const tokens = await generateTokens(payload.userId, payload.roles);
 
   res
-    .cookie("refreshToken", newRefreshToken, {
+    .cookie("accessToken", tokens.accessToken, {
       httpOnly: true,
       secure: true,
       sameSite: "Strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      maxAge: 10 * 60 * 1000,
+    })
+    .cookie("refreshToken", tokens.refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "Strict",
+      maxAge: 10 * 24 * 60 * 60 * 1000,
     })
     .status(200)
-    .json({ accessToken, message: "Access token refreshed." });
+    .json({ accessToken, message: "Access token refreshed successfully." });
 });
 
 export const logout = asyncHandler(async (req, res) => {
