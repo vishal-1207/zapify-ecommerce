031b5b734a4e2d45246c416b4516d0b68215456d
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-07-06 23:29:30 +0530

    added social auth routes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -10,6 +10,7 @@ import rateLimit from "express-rate-limit";
 import { csrfProtection } from "../middleware/csrf.middleware.js";
 import { validate } from "../middleware/validate.middleware.js";
 import { loginSchema, registerSchema } from "../utils/validationSchema.js";
+import passport from "passport";
 
 const router = express.Router();
 
@@ -19,13 +20,38 @@ const refreshLimiter = rateLimit({
   message: "Too many requests.",
 });
 
+//Register route
 router
   .route("/register")
   .post(csrfProtection.bind, validate(registerSchema), register);
+//TODO: complete seller registation
+router.route("/seller/register");
 
+//Login routes
 router.route("/admin/login").post(csrfProtection, validate(loginSchema), login);
-
 router.route("/login").post(csrfProtection, login);
+
+//Social auth routes
+router
+  .route("/google")
+  .get(passport.authenticate("google", { scope: ["profile", "email"] }));
+router
+  .route("/google/callback")
+  .get(
+    passport.authenticate("google", { session: false }),
+    socialCallbackHandler
+  );
+
+// GitHub OAuth
+router
+  .route("/github")
+  .get(passport.authenticate("github", { scope: ["user:email"] }));
+router
+  .route("/github/callback")
+  .get(
+    passport.authenticate("github", { session: false }),
+    socialCallbackHandler
+  );
 
 router
   .route("/access-token")
