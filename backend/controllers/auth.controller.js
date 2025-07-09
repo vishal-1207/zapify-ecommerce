218e875071483788f0d59286213ccc1858c759f6
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-07-09 23:30:53 +0530

    added social oauth callback handler controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,7 +3,10 @@ import { createUser, findUser } from "../services/auth.service.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken";
 import ApiError from "../utils/ApiError.js";
+import generateTokens from "../utils/token.utils.js";
+import setTokensInCookies from "../utils/setTokensInCookies.js";
 
+//Register Controller
 export const register = asyncHandler(async (req, res) => {
   const { fullname, username, email, password } = req.body;
   const { user, accessToken } = await createUser(
@@ -19,12 +22,31 @@ export const register = asyncHandler(async (req, res) => {
   res.status(201).json({ message: "User registered", user, accessToken });
 });
 
+//Login Controller
 export const login = asyncHandler(async (req, res) => {
   const { userId, password } = req.body;
   const { user, accessToken } = await findUser({ userId, password }, res);
   res.status(200).json({ message: "Login successfull", user, accessToken });
 });
 
+//Social Callback Handler Controller
+export const socialCallbackHandler = asyncHandler(async (req, res) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new ApiError(500, "Social oauth request failed.");
+  }
+
+  const tokens = await generateTokens(user.userId, user.roles);
+  setTokensInCookies(res, tokens);
+  res.status(200).json({
+    message: "Login successful.",
+    user,
+    accessToken: tokens.accessToken,
+  });
+});
+
+// Token Handler Controller
 export const refreshTokenHander = asyncHandler(async (req, res) => {
   const oldRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
   if (!oldRefreshToken) {
@@ -41,7 +63,7 @@ export const refreshTokenHander = asyncHandler(async (req, res) => {
   await storedToken.destroy();
 
   const tokens = await generateTokens(payload.userId, payload.roles);
-
+  setTokensInCookies(res, { ...tokens });
   res
     .cookie("accessToken", tokens.accessToken, {
       httpOnly: true,
@@ -59,6 +81,7 @@ export const refreshTokenHander = asyncHandler(async (req, res) => {
     .json({ accessToken, message: "Access token refreshed successfully." });
 });
 
+//Logout Controller
 export const logout = asyncHandler(async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
 
