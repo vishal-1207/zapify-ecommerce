91a048eafb5fcc3d52e4d17bc3d825f44bdf9c0a
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-07-23 23:42:03 +0530

    added roles authorization middleware in routes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,14 +1,25 @@
 import express from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import { currentUserDetails } from "../controllers/user.controller.js";
+import {
+  currentUserDetails,
+  deleteUser,
+} from "../controllers/user.controller.js";
 import { csrfProtection } from "../middleware/csrf.middleware.js";
 import { updateProfile } from "../controllers/user.controller.js";
+import { authorizeRoles } from "../middleware/authorizeRoles.middleware.js";
 
 const router = express.Router();
 
-router.route("/profile").get(authenticate, currentUserDetails);
+router
+  .route("/profile")
+  .get(authenticate, authorizeRoles("user"), currentUserDetails);
+
 router
   .route("/profile/edit")
-  .patch(authenticate, csrfProtection, updateProfile);
+  .patch(authenticate, authorizeRoles("user"), csrfProtection, updateProfile);
+
+router
+  .route("/profile/delete")
+  .delete(authenticate, authorizeRoles("user"), csrfProtection, deleteUser);
 
 export default router;
