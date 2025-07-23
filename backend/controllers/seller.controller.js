9875cd3f9424d55b4cb1378c8dce6b2f7a5a46dd
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-07-23 23:39:53 +0530

    added update seller profile controller

--- backend/controllers/seller.controller.js
+++ backend/controllers/seller.controller.js
@@ -3,8 +3,6 @@ import db from "../models/index.js";
 import { registerSellerService } from "../services/seller.service.js";
 import asyncHandler from "../utils/asyncHandler";
 
-const SellerProfile = db.SellerProfile;
-
 export const registerSellerProfile = asyncHandler(async (req, res) => {
   const {
     storeName,
@@ -23,10 +21,33 @@ export const registerSellerProfile = asyncHandler(async (req, res) => {
     .json({ message: "Seller profile created successfully.", seller });
 });
 
-export const updateSellerProfile = asyncHandler(async (req, res) => {});
+export const updateSellerProfile = asyncHandler(async (req, res) => {
+  const {
+    storeName,
+    contactNumber,
+    address,
+    bio = null,
+    website = null,
+  } = req.body;
+
+  const userId = req.user.id;
+
+  const data = { storeName, contactNumber, address, userId };
+  const optional = { bio, website };
+
+  //TODO: complete update seller profile service
+  const updatedSeller = await updateSellerService(data, optional);
+
+  res.status(201).json({
+    message: "Seller profile updated successfully.",
+    seller: updatedSeller,
+  });
+});
 
 export const deleteSellerProfile = asyncHandler(async (req, res) => {});
 
 export const getSellerProfile = asyncHandler(async (req, res) => {});
 
 export const getSellerAnalytics = asyncHandler(async (req, res) => {});
+
+export const otpVerification = asyncHandler(async (req, res) => {});
