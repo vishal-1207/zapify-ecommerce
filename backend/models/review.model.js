93f0938c2f94898d20487e61ee1bd5f2ac6797c6
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-08-06 23:56:42 +0530

    made changes

--- backend/models/review.model.js
+++ backend/models/review.model.js
@@ -5,11 +5,6 @@ export default (sequelize, DataTypes) => {
     "Review",
     {
       id: { type: DataTypes.UUID, defaultValue: UUIDV4, primaryKey: true },
-      productId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: { model: "Products", key: "id" },
-      },
       rating: { type: DataTypes.INTEGER, allowNull: false },
       comment: { type: DataTypes.TEXT, allowNull: true },
     },
