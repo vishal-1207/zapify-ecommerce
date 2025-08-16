ddbc36a8dfb71a95f612a68b3a8622113abcc968
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-08-17 01:44:22 +0530

    added joi validation for search

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { validate } from "../middleware/validate.middleware";
+import { searchSchema } from "../utils/validationSchema";
 
 const router = express.Router();
 
-router.route("/q").get(searchProducts);
+router.route("/q").get(validate(searchSchema), searchProducts);
 
 export default router;
