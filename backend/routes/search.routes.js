1a79ddc5ebdd63cf333e4e4b9c1475411b20545e
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-08-22 21:20:43 +0530

    fixed controller name in route

--- backend/routes/search.routes.js
+++ backend/routes/search.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { validate } from "../middleware/validate.middleware";
-import { searchSchema } from "../utils/validationSchema";
+import { validate } from "../middleware/validate.middleware.js";
+import { searchSchema } from "../utils/validationSchema.js";
+import { getSearchResults } from "../controllers/search.controller.js";
 
 const router = express.Router();
 
-router.route("/q").get(validate(searchSchema), searchProducts);
+router.route("/q").get(validate(searchSchema), getSearchResults);
 
 export default router;
