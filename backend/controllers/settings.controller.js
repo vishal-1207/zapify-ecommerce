d9d764152c5219bf00821dcb671dae794df397af
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-08-24 01:01:45 +0530

    made typo changes

--- backend/controllers/settings.controller.js
+++ backend/controllers/settings.controller.js
@@ -2,11 +2,11 @@ import db from "../models/index.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { userSettingsSchema } from "../utils/validationSchema.js";
 
-const UserSetting = db.UserSetting;
+const UserSettings = db.UserSettings;
 
 export const getUserSettings = aynscHandler(async (req, res) => {
   const userId = req.user.id;
-  const settings = await UserSetting.findOne({ where: { userId } });
+  const settings = await UserSettings.findOne({ where: { userId } });
   if (!settings) throw new ApiError(404, "Settings not found for this user.");
   res.json({ message: "User settings fetched successfully.", settings });
 });
@@ -15,7 +15,7 @@ export const updateUserSettings = asyncHandler(async (req, res) => {
   const userId = req.user.id;
   const settingsData = req.body;
 
-  const settings = await UserSetting.findOne({ where: { userId } });
+  const settings = await UserSettings.findOne({ where: { userId } });
   if (!settings) throw new ApiError(404, "Settings not found for this user.");
   await settings.update(settingsData);
   res.json({ message: "User settings updated successfully.", settings });
