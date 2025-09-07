06adcdd181c82fdf1a96843ef659c471b12019cb
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-09-07 23:13:21 +0530

    added shipment model

---
+++ backend/models/shipment.model.js
@@ -0,0 +1,42 @@
+export default (sequelize, DataTypes) => {
+  const Shipment = sequelize.define("Shipment", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    trackingNumber: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    shippingCarrier: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM(
+        "Processing",
+        "Shipped",
+        "In Transit",
+        "Delivered",
+        "Cancelled"
+      ),
+      defaultValue: "Processing",
+      allowNull: false,
+    },
+  });
+
+  Shipment.associate = (models) => {
+    Shipment.belongsTo(models.Order, {
+      foreignKey: "orderId",
+      onDelete: "CASCADE",
+    });
+
+    Shipment.belongsTo(models.SellerProfileId, {
+      foreignKey: "sellerProfileId",
+      onDelete: "SET NULL",
+    });
+  };
+
+  return Shipment;
+};
