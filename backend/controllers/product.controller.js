e066627fe437061a48c68af180ab9047b2946ec4
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-09-17 23:27:34 +0530

    added search catalog and admin controller for fetching product details, and updated few controllers

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,21 +1,15 @@
-import {
-  createProductService,
-  deleteProductService,
-  updateProductService,
-} from "../services/product.service.js";
+import * as productService from "../services/product.service.js";
 import ApiError from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
+/**
+ * Helper function for checking and parsing input data to process for services.
+ * @param {*} req
+ * @param {*} isUpdate
+ * @returns datat - parsed json data for product
+ */
 const parseProductInput = (req, isUpdate = false) => {
-  const {
-    categoryId,
-    brand,
-    name,
-    description,
-    price,
-    stock,
-    specs = [],
-  } = req.body;
+  const { categoryId, brand, name, description, price, specs = [] } = req.body;
   const thumbnail = req.files?.thumbnail?.[0];
   const gallery = req.files?.gallery;
 
@@ -41,7 +35,6 @@ const parseProductInput = (req, isUpdate = false) => {
       name,
       description,
       price: parseInt(price, 10),
-      stock: parseInt(stock, 10),
       specs: parsedSpecs,
     },
     files: {
@@ -51,22 +44,74 @@ const parseProductInput = (req, isUpdate = false) => {
   };
 };
 
-// GET PRODUCT DETAILS
-export const getProductDetails = asyncHandler(async (req, res) => {
-  const { id, slug } = req.params;
-  const identifier = id ? { id } : { slug };
+/**
+ * Get method controller to fetch details for a product, which is presented to user on front end.
+ */
+export const getProductDetailsForCustomer = asyncHandler(async (req, res) => {
+  const productSlug = req.params;
 
-  const product = await getProductDetailsService(identifier);
+  const product = await productService.getCustomerProductDetails(productSlug);
   return res
     .status(200)
     .json({ message: "Product details fetched successfully.", product });
 });
 
-// CREATE PRODUCT CONTROLLER
+/**
+ * Get method controller for admin to view/audit the product details.
+ */
+export const getProductDetailsAdmin = asyncHandler(async (req, res) => {
+  const productId = req.params;
+
+  const product = await productService.getProductDetailsAdmin(productId);
+
+  if (!product) {
+    throw new ApiError(404, "Product not found.");
+  }
+
+  return res
+    .status(200)
+    .json({ message: "Product details fetched succesfully.", product });
+});
+
+/**
+ * Seller controller to suggest product to admin that is not listed or available on the platform.
+ */
+export const suggestNewProduct = asyncHandler(async (req, res) => {});
+
+/**
+ * Admin controller to get list of pending suggested products by seller.
+ */
+export const getPendingProductsForReview = asyncHandler(async (req, res) => {});
+
+/**
+ * Admin controller to review product, and perform needed action based on product data.
+ */
+export const reviewProduct = asyncHandler(async (req, res) => {});
+
+/**
+ * Seller controller for finding product from list of active products listed by admin.
+ */
+export const searchCatalog = asyncHandler(async (req, res) => {
+  const searchTerm = req.query;
+
+  const searchResult = await productService.searchProductCatalog(searchTerm);
+
+  if (!searchResult) {
+    throw new ApiError("No such record found.");
+  }
+
+  return res
+    .status(200)
+    .json({ message: `Search result for ${searchTerm}.`, searchResult });
+});
+
+/**
+ * Admin controller for creating or adding products to the product list. By default the product is added to the listed products. And is available for seller to put offer for that product.
+ */
 export const createProduct = asyncHandler(async (req, res) => {
   const { data, files } = parseProductInput(req);
   const { createdProduct, productSpecs, thumbnailImage, galleryImages } =
-    await createProductService(data, files);
+    await productService.createProductService(data, files);
 
   res.status(200).json({
     message: "Product added successfully.",
@@ -77,14 +122,16 @@ export const createProduct = asyncHandler(async (req, res) => {
   });
 });
 
-// UPDATE PRODUCT CONTROLLER
+/**
+ * Admin controller for updating details for listed products.
+ */
 export const updateProduct = asyncHandler(async (req, res) => {
   const productId = parseInt(req.params.id, 10);
   if (isNaN(productId)) throw new ApiError(400, "Invalid product Id");
 
   const { data, files } = parseProductInput(req, true);
   const { updatedProduct, updatedSpecs, updatedThumbnail, updaterGallery } =
-    await updateProductService(productId, data, files);
+    await productService.updateProductService(productId, data, files);
 
   res.status(200).json({
     message: "Product updated successfully.",
@@ -95,9 +142,11 @@ export const updateProduct = asyncHandler(async (req, res) => {
   });
 });
 
-// DELETE PRODUCT CONTROLLER
+/**
+ * Admin controller for deleting product from the list of available products.
+ */
 export const deleteProduct = asyncHandler(async (req, res) => {
   const productId = req.params.id;
-  const result = await deleteProductService(productId);
+  const result = await productService.deleteProductService(productId);
   res.status(200).json({ message: result.message });
 });
