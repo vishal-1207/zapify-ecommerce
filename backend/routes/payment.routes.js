2aa8c35a1909b1e22f9a737aa387ab80f1c48d69
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-04 23:50:06 +0530

    added payment routes

---
+++ backend/routes/payment.routes.js
@@ -0,0 +1,24 @@
+import * as paymentControllers from "../controllers/payment.controller.js";
+import { authenticate } from "../middleware/auth.middleware.js";
+import express from "express";
+import { csrfProtection } from "../middleware/csrf.middleware.js";
+
+const router = express.Router();
+router.use(authenticate);
+
+/**
+ * Payment route for creating payment intent
+ */
+router
+  .route("/create-intent")
+  .post(csrfProtection, paymentControllers.createPaymentIntent);
+
+router
+  .route("/webhook")
+  .post(
+    csrfProtection,
+    express.raw({ type: "application/json" }),
+    paymentControllers.stripeWebhookHandler
+  );
+
+export default router;
