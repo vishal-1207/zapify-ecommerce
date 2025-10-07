f228c6456b7604fd4f7ec29383ede22f3dedbac1
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-07 23:54:28 +0530

    updated otp controleers as per email and phone number method

--- backend/controllers/otp.controller.js
+++ backend/controllers/otp.controller.js
@@ -1,15 +1,46 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import * as otpServices from "../services/otp.service.js";
 
-export const sendOtpController = asyncHandler(async (req, res) => {
-  const userId = req.user.id;
-  const result = await otpServices.sendVerificationOtp(userId);
-  return res.status(200).json({ message: result.message, result });
+// To send the code to the user's phone
+export const sendPhoneVerificationController = asyncHandler(
+  async (req, res) => {
+    const result = await otpServices.sendVerificationCode(req.user.id, "sms");
+    return res.status(200).json({ message: result.message, result });
+  }
+);
+
+// To verify the code and update the phone status
+export const verifyPhoneController = asyncHandler(async (req, res) => {
+  const { code } = req.body;
+  await otpServices.verifyCode(req.user.id, code);
+
+  await db.User.update(
+    { isPhoneVerified: true },
+    { where: { id: req.user.id } }
+  );
+
+  return res
+    .status(200)
+    .json({ message: "Phone number verified successfully." });
 });
 
-export const verifyOtpController = asyncHandler(async (req, res) => {
-  const userId = req.user.id;
+// To send the code to the user's email
+export const sendEmailVerificationController = asyncHandler(
+  async (req, res) => {
+    const result = await otpServices.sendVerificationCode(req.user.id, "email");
+    return res.status(200).json({ message: result.message, result });
+  }
+);
+
+// To verify the code and update the email status
+export const verifyEmailController = asyncHandler(async (req, res) => {
   const { code } = req.body;
-  const result = await verifyOtp(userId, code);
-  return res.status(200).json({ message: result.message, result });
+  await otpServices.verifyCode(req.user.id, code);
+
+  await db.User.update(
+    { isEmailVerified: true },
+    { where: { id: req.user.id } }
+  );
+
+  return res.status(200).json({ message: "Email verified successfully." });
 });
