0b6bc1e31eea1ff0df5a4ac3dfe49a1387f08bf9
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-08 21:23:01 +0530

    updated otp endpoints

--- backend/routes/otp.routes.js
+++ backend/routes/otp.routes.js
@@ -1,17 +1,26 @@
 import express from "express";
 import * as otpControllers from "../controllers/otp.controller.js";
 import authenticate from "../middleware/auth.middleware.js";
+import { csrfProtection } from "../middleware/csrf.middleware.js";
+import { validate } from "../middleware/validate.middleware.js";
+import { verifyCodeSchema } from "../utils/validationSchema.js";
 
 const router = express.Router();
 router.use(authenticate);
 
 router
-  .route("/-send-phone-code")
+  .route("/send-code/phone")
   .post(otpControllers.sendPhoneVerificationController);
-router.route("/verify-phone").post(otpControllers.verifyPhoneController);
+router.route("/phone/verify").post(otpControllers.verifyPhoneController);
 router
-  .route("/send-email-code")
+  .route("/send-code/email")
   .post(otpControllers.sendEmailVerificationController);
-router.route("/verify-email").post(otpControllers.verifyEmailController);
+router
+  .route("/email/verify")
+  .post(
+    csrfProtection,
+    validate(verifyCodeSchema),
+    otpControllers.verifyEmailController
+  );
 
 export default router;
