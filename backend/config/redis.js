a8101a750c559da586ba660ad6d801933ccc9074
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-09 18:11:03 +0530

    added redis configration

---
+++ backend/config/redis.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { createClient } from "redis";
+
+const redisPort = parseInt(process.env.REDIS_PORT, 10) || 15226;
+
+const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: redisPort,
+  },
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+(async () => {
+  try {
+    await redisClient.connect();
+    console.log("Successfully connected to Redis Cloud.");
+  } catch (err) {
+    console.error("Failed to connect to Redis Cloud:", err);
+  }
+})();
+
+export default redisClient;
