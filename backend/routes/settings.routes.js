5455375364284d5d62101981f0b348e5620a33ae
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-09 18:12:27 +0530

    updated settings routes

--- backend/routes/settings.routes.js
+++ backend/routes/settings.routes.js
@@ -2,13 +2,14 @@ import express from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
 import { validate } from "../middleware/validate.middleware.js";
 import { userSettingsSchema } from "../utils/validationSchema.js";
+import * as settingControllers from "../controllers/settings.controller.js";
 
 const router = express.Router();
+router.use(authenticate);
 
-// TODO: COMPLETE CONTROLLER FOR BELOW ROUTES
-// router
-//   .route("/")
-//   .get(authenticate, getUserSettings)
-//   .patch(authenticate, validate(userSettingsSchema), updateUserSettings);
+router
+  .route("/")
+  .get(settingControllers.getUserSettings)
+  .patch(validate(userSettingsSchema), settingControllers.updateUserSettings);
 
 export default router;
