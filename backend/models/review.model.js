3456bb777ea8f0228097655edd09679b49d8891e
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-10 22:26:01 +0530

    updated review model schema

--- backend/models/review.model.js
+++ backend/models/review.model.js
@@ -1,38 +1,33 @@
 export default (sequelize, DataTypes) => {
-  const Review = sequelize.define(
-    "Review",
-    {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      rating: { type: DataTypes.INTEGER, allowNull: false },
-      comment: { type: DataTypes.TEXT, allowNull: true },
+  const Review = sequelize.define("Review", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
     },
-    {
-      indexes: [
-        {
-          unique: true,
-          fields: ["userId", "productId"],
-        },
-      ],
-      tableName: "Reviews",
-      timestamps: true,
-    }
-  );
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: { min: 1, max: 5 },
+    },
+    comment: { type: DataTypes.TEXT, allowNull: true },
+    orderItemId: { type: DataTypes.UUID, allowNull: false, unique: true },
+  });
 
   Review.associate = (models) => {
     Review.belongsTo(models.User, { foreignKey: "userId" });
     Review.belongsTo(models.Product, {
       foreignKey: "productId",
-      as: "product",
+    });
+    Review.belongsTo(models.OrderItem, {
+      foreignKey: "orderItemId",
     });
     Review.hasMany(models.Media, {
       foreignKey: "associatedId",
       constraints: false,
       scope: { associatedType: "review" },
       as: "media",
+      onDelete: "CASCADE",
     });
   };
 
