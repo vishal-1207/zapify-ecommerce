82b658f8b38a4fe22ff3cf5c4f41f4b71029d82e
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-10-23 18:26:43 +0530

    updated setting routes for patch

--- backend/routes/settings.routes.js
+++ backend/routes/settings.routes.js
@@ -1,15 +1,17 @@
 import express from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import { validate } from "../middleware/validate.middleware.js";
-import { userSettingsSchema } from "../utils/validationSchema.js";
 import * as settingControllers from "../controllers/settings.controller.js";
+import { authorizeRoles } from "../middleware/authorizeRoles.middleware.js";
 
 const router = express.Router();
 router.use(authenticate);
 
 router
-  .route("/")
-  .get(settingControllers.getUserSettings)
-  .patch(validate(userSettingsSchema), settingControllers.updateUserSettings);
+  .route("/user/:username")
+  .patch(authorizeRoles("user"), settingControllers.updateUserSettings);
+
+router
+  .route("/seller/:sellerName")
+  .patch(authorizeRoles("seller"), settingControllers.updateSellerSettings);
 
 export default router;
