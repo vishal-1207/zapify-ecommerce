99b69d1193ab4b8a85e5bd53e7aadb8e5f47d1d5
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-11-02 22:33:43 +0530

    made changes to notification routes imported modules

--- backend/routes/notification.routes.js
+++ backend/routes/notification.routes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import { authenticate } from "../middleware/auth.middleware.js";
-import {
-  getMyNotifications,
-  markAsRead,
-  clearAll,
-} from "../controllers/notification.controller.js";
+import * as notificationController from "../controllers/notification.controller.js";
 
 const router = express.Router();
 router.use(authenticate);
 
-router.route("/").get(getMyNotifications).patch(markAsRead).delete(clearAll);
+router
+  .route("/")
+  .get(notificationController.getMyNotifications)
+  .patch(notificationController.markAsRead)
+  .delete(notificationController.clearAll);
 
 export default router;
