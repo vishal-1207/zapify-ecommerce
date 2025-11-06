ca984e6bdd6d2d03c2133d2fbd67cbcf8f6aaeb1
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-11-06 23:55:12 +0530

    made changes

--- backend/routes/otp.routes.js
+++ backend/routes/otp.routes.js
@@ -8,10 +8,10 @@ import { verifyCodeSchema } from "../utils/validationSchema.js";
 const router = express.Router();
 router.use(authenticate);
 
-router
-  .route("/send-code/phone")
-  .post(otpControllers.sendPhoneVerificationController);
-router.route("/phone/verify").post(otpControllers.verifyPhoneController);
+// router
+//   .route("/send-code/phone")
+//   .post(otpControllers.sendPhoneVerificationController);
+// router.route("/phone/verify").post(otpControllers.verifyPhoneController);
 router
   .route("/send-code/email")
   .post(otpControllers.sendEmailVerificationController);
