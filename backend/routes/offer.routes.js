1f8b7dab7d9375dc8b8d2f63da50938d15d2cdbf
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-11-06 23:54:55 +0530

    updated offer routes fixed ordering of midlleware

--- backend/routes/offer.routes.js
+++ backend/routes/offer.routes.js
@@ -5,6 +5,7 @@ import { offerSchema } from "../utils/validationSchema.js";
 import { validate } from "../middleware/validate.middleware.js";
 import * as offerController from "../controllers/offer.controller.js";
 import * as productController from "../controllers/product.controller.js";
+import { csrfProtection } from "../middleware/csrf.middleware.js";
 
 const router = express.Router();
 router.use(authenticate);
@@ -13,6 +14,7 @@ router
   .route("/product/:productId")
   .post(
     authorizeRoles("seller"),
+    csrfProtection,
     validate(offerSchema),
     offerController.createOffer
   );
@@ -22,9 +24,14 @@ router
   .get(authorizeRoles("seller"), productController.getProductOfferDetails)
   .patch(
     authorizeRoles("seller"),
+    csrfProtection,
     validate(offerSchema),
     offerController.updateProductOffer
   )
-  .delete(authorizeRoles("seller"), offerController.deleteOffer);
+  .delete(
+    authorizeRoles("seller"),
+    csrfProtection,
+    offerController.deleteOffer
+  );
 
 export default router;
