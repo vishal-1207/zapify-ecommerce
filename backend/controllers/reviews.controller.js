0f7d615280ab36cd282195742fd799ec8db7f7b2
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-11-09 22:00:48 +0530

    fixed import statement

--- backend/controllers/reviews.controller.js
+++ backend/controllers/reviews.controller.js
@@ -1,4 +1,4 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 import * as reviewServices from "../services/reviews.service.js";
 
 /**
