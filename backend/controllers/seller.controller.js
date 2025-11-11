4b34a0977a2ca0eefb515ea243d65e34cb8cab6f
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-11-12 00:31:08 +0530

    updated seller controller removed address from seller profile model

--- backend/controllers/seller.controller.js
+++ backend/controllers/seller.controller.js
@@ -3,16 +3,10 @@ import asyncHandler from "../utils/asyncHandler";
 
 // Seller profile registration controller
 export const createProfile = asyncHandler(async (req, res) => {
-  const {
-    storeName,
-    contactNumber,
-    address,
-    bio = null,
-    website = null,
-  } = req.body;
+  const { storeName, contactNumber, bio = null, website = null } = req.body;
   const userId = req.user.id;
 
-  const data = { storeName, contactNumber, address, userId };
+  const data = { storeName, contactNumber, userId };
   const optional = { bio, website };
   const seller = await sellerService.createSellerProfile(data, optional);
   res
@@ -30,17 +24,11 @@ export const getProfile = asyncHandler(async (req, res) => {
 
 // Update seller profile controller
 export const updateProfile = asyncHandler(async (req, res) => {
-  const {
-    storeName,
-    contactNumber,
-    address,
-    bio = null,
-    website = null,
-  } = req.body;
+  const { storeName, contactNumber, bio = null, website = null } = req.body;
 
   const slug = req.params.slug;
 
-  const data = { storeName, contactNumber, address, slug };
+  const data = { storeName, contactNumber, slug };
   const optional = { bio, website };
 
   const updatedSeller = await sellerService.updateSellerProfile(data, optional);
