cbe0d15fe0863e2d52a1443031c4342e25a58c6c
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-12-13 22:16:17 +0530

    imported routes and added redis connection

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,32 @@ import db from "./models/index.js";
 import express from "express";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import redisClient from "./config/redis.js";
 import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
 import adminRoutes from "./routes/admin.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import sellerRoutes from "./routes/seller.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import brandRoutes from "./routes/brand.routes.js";
-import { errorHandler } from "./middleware/error.middleware.js";
+import errorHandler from "./middleware/error.middleware.js";
 import tokenRoutes from "./routes/token.routes.js";
-import userSettingsRoutes from "./routes/settings.routes.js";
+import settingsRoutes from "./routes/settings.routes.js";
+import addressRoutes from "./routes/address.routes.js";
 import searchRoutes from "./routes/search.routes.js";
 import offerRoutes from "./routes/offer.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+import reviewRoutes from "./routes/review.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import paymentRoutes from "./routes/payment.routes.js";
 import { startCleanupService } from "./services/cleanup.service.js";
+import otpRoutes from "./routes/otp.routes.js";
+import notificationRoutes from "./routes/notification.routes.js";
 import passport from "passport";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 app.use(helmet());
 app.use(cookieParser());
@@ -30,17 +39,20 @@ app.use("/api/auth", authRoutes);
 app.use("/api/token", tokenRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/notification", notificationRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/brand", brandRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/offer", offerRoutes);
-// app.use("/api/cart", cartRoutes);
-// app.use("/api/order", orderRoutes);
-// app.use("/api/payment", paymentRoutes);
-// app.use("/api/seller", sellerRoutes);
-// app.use("/api/dashboard", dashboardRoutes);
-app.use("/api/settings", userSettingsRoutes);
+app.use("/api/review", reviewRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/settings", settingsRoutes);
 app.use("/api/search", searchRoutes);
+app.use("/api/otp", otpRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello world.");
@@ -50,7 +62,12 @@ app.use(errorHandler);
 
 const startServer = async () => {
   try {
-    await db.sequelize.sync({ force: false });
+    // Test Redis connection
+    await redisClient.ping();
+    console.log("Redis client connected...");
+
+    // Sync SQL database
+    await db.sequelize.sync({ force: true });
     console.log("Database synced...");
 
     startCleanupService();
