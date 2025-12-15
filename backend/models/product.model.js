af45c76ffeaa293afb0883fe3c196729bf79f9aa
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-12-15 18:04:36 +0530

    added hooks for after create, update and destroy to update and sync algolia

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,49 +1,69 @@
 import { nanoid } from "nanoid";
 import { Op } from "sequelize";
 import slugify from "slugify";
+import { syncProductToAlgolia } from "../services/algolia.service";
 
 export default (sequelize, DataTypes) => {
-  const Product = sequelize.define("Product", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    name: { type: DataTypes.STRING, allowNull: false },
-    model: { type: DataTypes.STRING, allowNull: true },
-    description: { type: DataTypes.TEXT, allowNull: false },
-    price: { type: DataTypes.DECIMAL(10, 2), allowNull: false }, // List price/MRP
-    status: {
-      type: DataTypes.ENUM("draft", "pending", "approved", "rejected"),
-      allowNull: false,
-      defaultValue: "pending",
-    },
-    averageRating: {
-      type: DataTypes.DECIMAL(3, 2),
-      allowNull: true,
-      defaultValue: 0.0,
-    },
-    reviewCount: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0,
-    },
-    slug: { type: DataTypes.STRING, allowNull: true, unique: true },
+  const Product = sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: { type: DataTypes.STRING, allowNull: false },
+      model: { type: DataTypes.STRING, allowNull: true },
+      description: { type: DataTypes.TEXT, allowNull: false },
+      price: { type: DataTypes.DECIMAL(10, 2), allowNull: false }, // List price/MRP
+      status: {
+        type: DataTypes.ENUM("draft", "pending", "approved", "rejected"),
+        allowNull: false,
+        defaultValue: "pending",
+      },
+      slug: { type: DataTypes.STRING, allowNull: true, unique: true },
 
-    // for search
-    minOfferPrice: { type: DataTypes.DECIMAL(10, 2), allowNull: true },
-    totalOfferStock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
+      averageRating: {
+        type: DataTypes.DECIMAL(3, 2),
+        allowNull: true,
+        defaultValue: 0.0,
+      },
+      reviewCount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      minOfferPrice: { type: DataTypes.DECIMAL(10, 2), allowNull: true },
+      totalOfferStock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      offerCount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      popularityScore: {
+        type: DataTypes.DOUBLE,
+        allowNull: false,
+        defaultValue: 0,
+      },
     },
-    offerCount: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-    popularityScore: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-      defaultValue: 0,
-    },
-  });
+    {
+      hooks: {
+        afterCreate: async (product) => {
+          syncProductToAlgolia(product.id).catch((e) => console.error(e));
+        },
+        afterUpdate: async (product) => {
+          syncProductToAlgolia(product.id).catch((e) => console.error(e));
+        },
+        afterDestroy: async (product) => {
+          deleteProductFromAlgolia(product.id).catch((e) => console.error(e));
+        },
+      },
+    }
+  );
 
   Product.beforeCreate(async (product, options) => {
     const baseSlug = slugify(product.name, { lower: true, strict: true });
