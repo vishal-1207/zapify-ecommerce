ea7ff133bbaab99a00d3d50e916ea502d300bfb9
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2025-12-18 23:11:38 +0530

    added hooks to keep aggregates upto date and synced properly after create, update and delete

--- backend/models/offer.model.js
+++ backend/models/offer.model.js
@@ -1,36 +1,54 @@
+import { updateProductAggregates } from "../services/product.service.js";
+
 export default (sequelize, DataTypes) => {
-  const Offer = sequelize.define("Offer", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
+  const Offer = sequelize.define(
+    "Offer",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      price: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        comment: "The price set by the seller for this offer.",
+      },
+      stockQuantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+        comment: "Live stock for this seller's offer.",
+      },
+      condition: {
+        type: DataTypes.ENUM(
+          "New",
+          "Used - New Like",
+          "Used - Good",
+          "Refurbished"
+        ),
+        defaultValue: "New",
+      },
+      status: {
+        type: DataTypes.ENUM("draft", "active"),
+        allowNull: false,
+        defaultValue: "draft",
+      },
     },
-    price: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      comment: "The price set by the seller for this offer.",
-    },
-    stockQuantity: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0,
-      comment: "Live stock for this seller's offer.",
-    },
-    condition: {
-      type: DataTypes.ENUM(
-        "New",
-        "Used - New Like",
-        "Used - Good",
-        "Refurbished"
-      ),
-      defaultValue: "New",
-    },
-    status: {
-      type: DataTypes.ENUM("draft", "active"),
-      allowNull: false,
-      defaultValue: "draft",
-    },
-  });
+    {
+      hooks: {
+        afterCreate: async (offer) => {
+          await updateProductAggregates(offer.productId);
+        },
+        afterUpdate: async (offer) => {
+          await updateProductAggregates(offer.productId);
+        },
+        afterDestroy: async (offer) => {
+          await updateProductAggregates(offer.productId);
+        },
+      },
+    }
+  );
 
   Offer.associate = (models) => {
     Offer.belongsTo(models.Product, {
