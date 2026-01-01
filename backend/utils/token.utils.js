2b647523545751a85af7f3a0ea3fe4e54f53ebfe
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2026-01-01 20:22:12 +0530

    refactored token utility function

--- backend/utils/token.utils.js
+++ backend/utils/token.utils.js
@@ -4,27 +4,31 @@ import jwt from "jsonwebtoken";
 
 const RefreshToken = db.RefreshToken;
 
-const generateTokens = async ({ userId, roles }) => {
-  const accessToken = jwt.sign({ userId, roles }, process.env.JWT_SECRET, {
+const generateTokens = async (user, transaction = null) => {
+  const payload = { id: user.id, roles: user.roles };
+  const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
   });
 
   const tokenId = uuid4();
   const expiresAt = new Date(Date.now() + 10 * 24 * 60 * 60 * 1000);
   const refreshToken = jwt.sign(
-    { tokenId, userId, roles },
+    { tokenId, ...payload },
     process.env.REFRESH_TOKEN_SECRET,
     {
       expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 
-  await RefreshToken.create({
-    tokenId,
-    userId,
-    token: refreshToken,
-    expiresAt,
-  });
+  await RefreshToken.create(
+    {
+      tokenId,
+      userId: user.id,
+      token: refreshToken,
+      expiresAt,
+    },
+    { transaction }
+  );
 
   return { accessToken, refreshToken };
 };
