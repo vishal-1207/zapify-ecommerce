88647343f4f33ed9d520913f0ba60808e2aa6b61
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2026-01-01 20:14:29 +0530

    added order discount model

---
+++ backend/models/orderDiscount.model.js
@@ -0,0 +1,31 @@
+export default (sequelize, DataTypes) => {
+  const OrderDiscounts = sequelize.define(
+    "OrderDiscounts",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      orderId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      discountId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      appliedAmount: {
+        type: DataTypes.DECIMAL(10, 2),
+        allowNull: false,
+        comment:
+          "Stores the exact amount saved at the moment of order placement.",
+      },
+    },
+    {
+      indexes: [{ fields: ["orderId"] }, { fields: ["discountId"] }],
+    }
+  );
+
+  return OrderDiscounts;
+};
