22fff1ceedbaf5648bf3fcde3c0772de444a1560
Vishal Chaudhary <vishalchaudhary1908@gmail.com>
2026-01-17 01:43:38 +0530

    made changes

--- backend/utils/setTokensInCookies.js
+++ backend/utils/setTokensInCookies.js
@@ -1,15 +1,11 @@
-const setTokensInCookies = (res, { accessToken, refreshToken }) => {
+const setTokensInCookies = (res, { accessToken, refreshToken }, options) => {
   res.cookie("accessToken", accessToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "Strict",
+    ...options,
     maxAge: 15 * 60 * 1000,
   });
 
   res.cookie("refreshToken", refreshToken, {
-    httpOnly: true,
-    secure: true,
-    sameSite: "Strict",
+    ...options,
     maxAge: 10 * 24 * 60 * 60 * 1000,
   });
 };
